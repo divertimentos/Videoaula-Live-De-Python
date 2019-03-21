b39591ccd641a2a291728df416ee8028581a0947
guiemi <guiemi.teixeira@gmail.com>
2019-03-21 14:33:05 -0300

    Adiciona um requirements ao repositorio

---
+++ live_10/bdd.py
@@ -0,0 +1,14 @@
+# BDD com Python
+
+"""
+Resumo da live:
+k- Como montar uma estória
+- Como testar uma estória
+- Como montar todo o ambiente
+- Conhecendo o Behave
+
+Metodologias ágeis:
+- Extreme Programming
+- Scrum
+
+"""
